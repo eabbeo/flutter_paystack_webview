b56261679ee66dfa28f1eb1ec742bec931409128
Edward Abbeo <abbeoedwardiisekou@gmail.com>
2023-10-29 23:29:33 +0100

    project set up

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Paystack Webview"),
+      ),
+      body: const Center(
+        child: Text('I am in the center'),
+      ),
+    );
+  }
+}
